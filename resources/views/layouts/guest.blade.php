21c65406683c795e7013b8af8fd353d292828067
GR34TNULL <gr34tnull@GR34TNULL.local>
2021-03-30 17:19:01 +0800

    test

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -40,23 +40,5 @@
         <div class="font-sans antialiased text-gray-900">
             {{ $slot }}
         </div>
-        <script>
-            window.fbAsyncInit = function() {
-                FB.init({
-                appId      : '196243415307074',
-                xfbml      : true,
-                version    : 'v10.0'
-                });
-                FB.AppEvents.logPageView();
-            };
-
-            (function(d, s, id){
-                var js, fjs = d.getElementsByTagName(s)[0];
-                if (d.getElementById(id)) {return;}
-                js = d.createElement(s); js.id = id;
-                js.src = "https://connect.facebook.net/en_US/sdk.js";
-                fjs.parentNode.insertBefore(js, fjs);
-            }(document, 'script', 'facebook-jssdk'));
-        </script>
     </body>
 </html>
