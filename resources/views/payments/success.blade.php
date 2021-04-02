630dd216d348e91469ecee30bc27401fc42905cf
GR34TNULL <gr34tnull@GR34TNULL.local>
2021-04-02 21:53:32 +0800

    payments

--- resources/views/payments/success.blade.php
+++ resources/views/payments/success.blade.php
@@ -2,6 +2,7 @@
 <div class="flex items-center justify-center w-screen h-screen bg-contain" style="background-image: url({{asset('bg.png')}})">
     <div class="flex flex-col items-center justify-center w-64 h-64 p-4 bg-white rounded-xl">
         <h1 class="text-xl font-extrabold text-center">Payment Success</h1>
+        <p class="text-xs text-center text-gray-900">We have received your payment. Your account will be processed and you should be able to use the service within 24 hours.</p>
     </div>
 </div>
 </x-app-layout>
